55bdb6f6fb657bc5aeb021f6b4dde8fcd218d50b
3omara <youssef.ahmed.ibrahim@outlook.com>
2024-09-04 22:42:26 +0300

    Built a Flow react custom node for the summary component.

---
+++ frontend/src/Components/SummaryNode.jsx
@@ -0,0 +1,65 @@
+import {
+  Handle,
+  Position,
+  useNodesData,
+  useHandleConnections,
+} from "@xyflow/react";
+import { useEffect, useState } from "react";
+import { stream_reader } from "../helpers";
+
+const summaryUrl = "http://127.0.0.1:5000/summarize";
+const default_summary = "Waiting for content to summarize...";
+export default function SummaryNode({}) {
+  const [summary, setSummary] = useState(default_summary);
+  const [content, setContent] = useState("");
+  const [subscribedNode, setSubscribedNode] = useState();
+
+  useHandleConnections({
+    type: "target",
+    onDisconnect: () => setSubscribedNode(""),
+    onConnect: (connection) => setSubscribedNode(connection[0].source),
+  });
+  const nodeData = useNodesData(subscribedNode);
+
+  useEffect(() => {
+    if (nodeData != null) {
+      nodeData.data.setcontent = setContent;
+    }
+  }, [nodeData]);
+
+  useEffect(() => {
+    const fetch_summary = async () => {
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ content }),
+      };
+      const response = await fetch(summaryUrl, options);
+      if (response.status !== 200) {
+        setSummary("Nothing to summarize.")
+      } else {
+        let result = await stream_reader(response)
+        setSummary(result.replace(/[^a-zA-Z0-9,. ]/g, "").slice(9, -2))
+      }
+    };
+    if(content !== ""){
+      fetch_summary();
+    }
+  }, [content]);
+
+  return (
+    <div className="Node" id="summaryNode">
+      <h2>Summary</h2>
+      <Handle type="target" position={Position.Left} />
+      <div className="verticalContainer">
+        <label htmlFor="text">Website Summary:</label>
+        <textarea
+          disabled
+          value={nodeData === null ? "No Webscraper node connected." : summary}
+        />
+      </div>
+    </div>
+  );
+}
