f439736f90f9d9a1b91d071bc354a4bcca9e2c1f
3omara <youssef.ahmed.ibrahim@outlook.com>
2024-09-04 22:43:47 +0300

    Created the canvas managing nodes and edges.

---
+++ frontend/src/Components/Canvas.jsx
@@ -0,0 +1,97 @@
+import React, { useMemo, useCallback } from "react";
+import {
+  ReactFlow,
+  ReactFlowProvider,
+  addEdge,
+  useNodesState,
+  useEdgesState,
+  useReactFlow,
+  Background,
+} from "@xyflow/react";
+import "@xyflow/react/dist/style.css";
+import WebScraperNode from "./WebScraperNode";
+import SummaryNode from "./SummaryNode";
+
+const initialNodes = [
+  {
+    id: "dndnode_1",
+    type: "webScraper",
+    position: { x: 0, y: 0 },
+    data: { label: "webScraper node" },
+  },
+  {
+    id: "dndnode_2",
+    type: "summary",
+    position: { x: 0, y: 100 },
+    data: { label: "summary node" },
+  },
+];
+
+let id = 3;
+const getId = () => `dndnode_${id++}`;
+
+const DnDFlow = ({ type }) => {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+  const { screenToFlowPosition } = useReactFlow();
+
+  const nodeTypes = useMemo(
+    () => ({ webScraper: WebScraperNode, summary: SummaryNode }),
+    []
+  );
+
+  const onConnect = useCallback(
+    (params) => setEdges((eds) => addEdge(params, eds)),
+    []
+  );
+
+  const onDragOver = useCallback((event) => {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = "move";
+  }, []);
+
+  const onDrop = useCallback(
+    (event) => {
+      event.preventDefault();
+
+      if (!type) {
+        return;
+      }
+      const position = screenToFlowPosition({
+        x: event.clientX,
+        y: event.clientY,
+      });
+      const newNode = {
+        id: getId(),
+        type,
+        position,
+        data: { label: `${type} node` },
+      };
+      setNodes((nds) => nds.concat(newNode));
+    },
+    [screenToFlowPosition, type]
+  );
+
+  return (
+    <div className="dndflow" style={{ width: "100vw", height: "100vh" }}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        onDrop={onDrop}
+        onDragOver={onDragOver}
+        nodeTypes={nodeTypes}
+        fitView
+      ></ReactFlow>
+    </div>
+  );
+};
+
+export default ({ type }) => (
+  <ReactFlowProvider>
+    <Background />
+    <DnDFlow type={type} />
+  </ReactFlowProvider>
+);
