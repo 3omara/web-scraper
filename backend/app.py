55fa8f0e8c07dcbc1e78c4f0333eeb9f23aadbb1
3omara <youssef.ahmed.ibrahim@outlook.com>
2024-09-04 22:39:15 +0300

    Minor changes and enhanced Gemini query.

--- backend/app.py
+++ backend/app.py
@@ -1,29 +1,33 @@
 from flask import request, jsonify 
 from config import app, model
-from scrapper import scrap_url
+from scraper import scrape_url
 
 @app.route("/")
 def home():
-    return "web-scrapper"
+    return "web-scraper"
 
 @app.route("/summarize", methods=['POST'])
 def summarize():
     content = request.json.get("content")
     if not content:
-        return jsonify({"message":"Missing content..."}), 400
+        return (jsonify({"message":"Missing content..."}), 400)
     
-    model_query = "Summarize in a concise and readable way: " + content
+    model_query = "Summarize in a concise and readable way without formatting or new lines: " + content
 
     summary = model.generate_content(model_query)
-    return jsonify({"summary": summary.text})
+    return jsonify({"summary": summary.text}), 200
 
-@app.route("/scrap", methods=['POST'])
-def scrap():
+@app.route("/scrape", methods=['POST'])
+def scrape():
     url = request.json.get("url")
     if not url:
-        return jsonify({"message":"Missing url..."}), 400
+        return (jsonify({"message":"Missing url..."}), 400)
     
-    return jsonify({"urlContent": scrap_url(url)})
+    result = scrape_url(url)
+    if result == "bad request":
+        return (jsonify({"message":result}), 400)
+    
+    return jsonify({"urlContent": result}), 200
 
 
 if __name__ == "__main__":
