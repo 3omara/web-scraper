1048287ec76279d61cf1f88f2bd9731862608122
3omara <youssef.ahmed.ibrahim@outlook.com>
2024-09-04 22:38:40 +0300

    Handled bad request exception.

---
+++ backend/scraper.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+import requests
+import html
+
+def scrape_url(url):
+    page = ''
+    
+    try:
+        page = requests.get(url)
+    except Exception as e:
+        return "bad request"
+    
+    soup = BeautifulSoup(page.text, 'html')
+
+    content = ''
+    for p in soup.find_all('p'):
+        content += BeautifulSoup(str(p), 'html').get_text()
+
+    return (html.unescape(content)).strip()
+
+######### Test ######### 
+#print(scrap_url("https://stackoverflow.com/questions/159720/what-is-the-naming-convention-in-python-for-variables-and-functions"))
